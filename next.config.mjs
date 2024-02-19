8a56617f0deb1d2b799afd4f8b80a01151970f59
Teemu Koivumaa <35847478+Teemukoivumaa@users.noreply.github.com>
2024-02-19 23:25:42 +0200

    feat: create github action to automatically ship page
    Builds next.js app correctly and ships it to GitHub pages with working
    CSS
    
    this took some fighting to get right

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+
+  // Add basePath
+  basePath: "/website",
+};
 
 export default nextConfig;
