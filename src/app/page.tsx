e3098718017b4c0eca952eae1610881247c5da13
Teemu Koivumaa <teemuk15@gmail.com>
2024-02-20 22:02:14 +0200

    fix: make theme switch bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,10 +24,10 @@ function ModeToggle() {
         <Button
           variant="outline"
           size="icon"
-          className="m-[0.5rem] absolute top-0 right-0"
+          className="m-[1rem] h-[2.8rem] w-[2.8rem] absolute top-0 right-0"
         >
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <SunIcon className="h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
